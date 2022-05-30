43c15072db440403a0baaabc545258c397708540
I_M <moskva.88@mail.ru>
2022-05-30 14:15:13 +0300

    5. Создания и редактирования кандидатов. [#504858]

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -7,9 +7,15 @@ public class Candidate {
     private int id;
     private String name;
     private String desc;
-    private LocalDateTime created;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Candidate() {
+    }
+
+    public Candidate(int id, String name, String desc) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
     }
 
     public Candidate(int id, String name, String desc, LocalDateTime created) {
--- src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CandidateStore {
 
@@ -13,10 +14,13 @@ public class CandidateStore {
 
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
+    private final AtomicInteger size;
+
     private CandidateStore() {
         candidates.put(1, new Candidate(1, "Ivan", "Java", LocalDateTime.now()));
         candidates.put(2, new Candidate(2, "Max", "C++", LocalDateTime.now()));
         candidates.put(3, new Candidate(3, "Anna", "Python", LocalDateTime.now()));
+        size = new AtomicInteger(candidates.size());
     }
 
     public static CandidateStore instOf() {
@@ -25,5 +29,18 @@ public class CandidateStore {
 
     public Collection<Candidate> findAll() {
         return candidates.values();
+    }
+
+    public Candidate add(Candidate candidate) {
+        candidate.setId(size.incrementAndGet());
+        return candidates.put(candidate.getId(), candidate);
+    }
+
+    public Candidate findById(int id) {
+        return candidates.get(id);
+    }
+
+    public boolean update(Candidate candidate) {
+        return candidates.replace(candidate.getId(), candidate) != null;
     }
 }
